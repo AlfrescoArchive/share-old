AlfrescoArchive/share-old

commit 35515a6b7da1d1818e16fd3d39a2af11debc0e64

git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/web-apps/Share/trunk@117510 7033f55c-6c29-4ad8-afba-765cb616e738

share/src/main/resources/alfresco/site-webscripts/org/alfresco/components/comments/comments-list.get.js
@@ -90,7 +90,8 @@ function main()
 
    var documentDetails = AlfrescoUtil.getNodeDetails(model.nodeRef, model.site);
    var activityParameters = null;
-   if (documentDetails)
+   var supressSocial = AlfrescoUtil.isComponentSuppressed(documentDetails.item.node, AlfrescoUtil.getSupressSocialfolderDetailsConfig());
+   if (documentDetails && !supressSocial)
    {
       activityParameters = getActivityParameters(model.nodeRef, null);
    }

commit 736a7f73769fdabe3d080b5e8050a146d325d44e

ACE-4917: Fixed null pointer exception caused by r117510.
git-svn-id: https://svn.alfresco.com/repos/alfresco-open-mirror/web-apps/Share/trunk@120514 7033f55c-6c29-4ad8-afba-765cb616e738

share/src/main/resources/alfresco/site-webscripts/org/alfresco/components/comments/comments-list.get.js
@@ -90,10 +90,13 @@ function main()
 
    var documentDetails = AlfrescoUtil.getNodeDetails(model.nodeRef, model.site);
    var activityParameters = null;
-   var supressSocial = AlfrescoUtil.isComponentSuppressed(documentDetails.item.node, AlfrescoUtil.getSupressSocialfolderDetailsConfig());
-   if (documentDetails && !supressSocial)
+   if (documentDetails)
    {
-      activityParameters = getActivityParameters(model.nodeRef, null);
+      var suppressSocial = AlfrescoUtil.isComponentSuppressed(documentDetails.item.node, AlfrescoUtil.getSupressSocialfolderDetailsConfig());
+      if(!suppressSocial)
+      {
+         activityParameters = getActivityParameters(model.nodeRef, null);
+      }
    }
    else
    {